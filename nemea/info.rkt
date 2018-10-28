0b37927acbd2e2a7d6eff67d3771ac2cb26ab212
Bogdan Popa <popa.bogdanp@gmail.com>
2018-10-28 10:59:07 +0200

    fix(build): add missing deps

--- nemea/info.rkt
+++ nemea/info.rkt
@@ -1,5 +1,10 @@
 #lang info
 
 (define collection "nemea")
-(define deps '("base" "db-lib" "gregor-lib" "threading-lib"))
-(define build-deps '("rackunit-lib"))
+(define deps '("base"
+               "db-lib"
+               "gregor-lib"
+               "threading-lib"
+               "web-server-lib"))
+(define build-deps '("at-exp-lib"
+                     "rackunit-lib"))
