03860df80702ac842d252e291edf66b5472dccb6
Bogdan Popa <popa.bogdanp@gmail.com>
2018-10-29 08:07:13 +0200

    feat(ui): mock up dashboard header

---
+++ assets/js/components/TotalsBox.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="totals-box" v-bind:class="{ 'totals-box--active': active }">
+    <h2 class="totals-box__value">{{ value }}</h2>
+    <h6 class="totals-box__label">{{ label }}</h6>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ["label", "value", "active"]
+  };
+</script>
+
+<style lang="scss">
+  .totals-box {
+    padding: 1rem;
+    font-size: 1.4rem;
+    opacity: 0.5;
+    transition: opacity 0.5s ease-in-out;
+
+    &:hover {
+      opacity: 0.9;
+    }
+
+    &--active,
+    &--active:hover {
+      opacity: 1;
+    }
+
+    &__value {
+      font-weight: 600;
+      text-transform: lowercase;
+    }
+  }
+
+  @media (min-width: 1024px) {
+    .totals-box {
+      padding: 1rem 0;
+    }
+  }
+</style>
--- assets/js/pages/Dashboard.vue
+++ assets/js/pages/Dashboard.vue
@@ -1,3 +1,33 @@
 <template>
-  <h1>Dashboard</h1>
+  <div class="container">
+    <div class="totals">
+      <totals-box label="Visits" value="8,351" active="true"></totals-box>
+      <totals-box label="Visitors" value="8,001"></totals-box>
+      <totals-box label="Avg. Time on Site" value="50s"></totals-box>
+      <totals-box label="Bounce Rate" value="30%"></totals-box>
+    </div>
+  </div>
 </template>
+
+<script>
+  import TotalsBox from "../components/TotalsBox.vue";
+
+  export default {
+    name: "Dashboard",
+    components: {TotalsBox}
+  };
+</script>
+
+<style type="scss">
+  .totals {
+    padding: 1rem;
+  }
+
+  @media (min-width: 1024px) {
+    .totals {
+      display: grid;
+      grid-template-columns: 1fr 1fr 1fr 1fr;
+      padding: 2rem 0;
+    }
+  }
+</style>
