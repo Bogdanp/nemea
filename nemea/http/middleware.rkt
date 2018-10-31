e968b4a42abccb70845b80bcdb5ef3071c920791
Bogdan Popa <popa.bogdanp@gmail.com>
2018-10-31 18:53:39 +0200

    refactor(logging): log http errors

--- nemea/http/middleware.rkt
+++ nemea/http/middleware.rkt
@@ -3,21 +3,23 @@
 (require "utils.rkt")
 
 (provide (struct-out exn:bad-request)
-         handle-custom-exns)
+         wrap-custom-exns)
+
+(define-logger http-error)
 
 (struct exn:bad-request exn:fail ())
 
 (define (bad-request->response e)
-  (log-warning "Encountered bad request: ~a" (exn-message e))
+  (log-http-error-warning "encountered bad request: ~a" (exn-message e))
   (response/json #:body (hasheq 'error (exn-message e))
                  #:code 400))
 
 (define (internal-error->response e)
-  (log-error "Unhandled error: ~a" (exn-message e))
+  (log-http-error-error "unhandled error: ~a" (exn-message e))
   (response/json #:body (hasheq 'error "internal server error")
                  #:code 500))
 
-(define ((handle-custom-exns app) req)
+(define ((wrap-custom-exns app) req)
   (with-handlers ([exn:bad-request? bad-request->response]
                   [exn:fail? internal-error->response])
     (app req)))
--- nemea/main.rkt
+++ nemea/main.rkt
@@ -39,6 +39,7 @@
      config:log-level 'app
      config:log-level 'batcher
      config:log-level 'database
+     config:log-level 'http-error
      config:log-level 'migrations
      config:log-level 'server
      config:log-level 'system))
