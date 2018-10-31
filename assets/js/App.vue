ad39df21d529150fcbafc0d1a48aab603d2a4ca0
Bogdan Popa <popa.bogdanp@gmail.com>
2018-10-31 08:47:18 +0200

    refactor(ui): drop vue runtime compiler

---
+++ assets/js/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="app">
+    <div class="nav">
+      <div class="container">
+        <ul class="nav__items">
+          <li class="nav__item nav__item--active">
+            <a href="#/">Dashboard</a>
+          </li>
+          <li class="nav__item">
+            <a href="#/settings">Settings</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="page">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {};
+</script>
--- assets/js/index.js
+++ assets/js/index.js
@@ -3,10 +3,11 @@ import VueRouter from "vue-router";
 
 Vue.use(VueRouter);
 
+import App from "./App.vue";
 import Dashboard from "./pages/Dashboard.vue";
 
 const routes = [{ path: "/", component: Dashboard }];
 const router = new VueRouter({ routes });
-const app = new Vue({ router });
+const app = new Vue({ render: h => h(App), router });
 
 app.$mount("#app");
